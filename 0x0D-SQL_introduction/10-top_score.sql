b8aff532a28b032df4b01ec638013ef65fed1bfc
Sowah <pilotsowah@live.com>
2022-08-13 08:27:31 -0700

    lists all records of the table second_table of the database

---
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -0,0 +1,5 @@
+-- listing all records in second_table of the database in my MYSQL server.
+-- records are ordered by descending score.
+SELECT `score`,`name`
+FROM `second_table`
+ORDER BY `score` DESC;
